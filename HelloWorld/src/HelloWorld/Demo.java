14d9631e3a2ddd50d1bcfac3c79e296cd2970346
akash-2614 <akashrawat2614@gmail.com>
2021-02-03 17:33:00 +0530

    Hello world program in java

---
+++ HelloWorld/src/HelloWorld/Demo.java
@@ -0,0 +1,14 @@
+package HelloWorld;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		byte age=20;
+		int count = 123_456_789;
+		long large = 123_456_789_754_335_678l;
+		System.out.print("Age = ");// TODO Auto-generated method stub
+		System.out.println(large);
+
+	}
+
+}
